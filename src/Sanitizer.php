b29ce49cb9264806b75c158d0fdab3ec2b1af9a4
Jeremy Desvaux <jeremy.desvaux@wonderful.fr>
2023-03-29 23:17:18 +0200

    feat(sanitize): better xss protection

--- src/Sanitizer.php
+++ src/Sanitizer.php
@@ -11,51 +11,56 @@ class Sanitizer implements SingletonInterface
         SingletonTrait::buildInstance as createInstance;
     }
 
-    public static function sanitize_text($data): string
+    public static function sanitizeText($data): string
     {
-        return sanitize_text_field($data);
+        // The default WordPress sanitization function
+        $data = sanitize_text_field($data);
+        // We also need to convert special characters to HTML entities
+        // because the sanitize_text_field function does not do it and it can be subject to XSS attacks
+        $data = htmlspecialchars($data, ENT_QUOTES, get_option('blog_charset'));
+        return $data;
     }
 
-    public static function sanitize_html($data): string
+    public static function sanitizeHtml($data): string
     {
         return wp_kses_post($data);
     }
 
-    public static function sanitize_email($data): string
+    public static function sanitizeEmail($data): string
     {
         return sanitize_email($data);
     }
 
-    public static function sanitize_url($data): string
+    public static function sanitizeUrl($data): string
     {
         return esc_url($data);
     }
 
-    public static function sanitize_integer($data): int
+    public static function sanitizeInteger($data): int
     {
         return absint($data);
     }
 
-    public static function sanitize_float($data): float
+    public static function sanitizeFloat($data): float
     {
         return floatval($data);
     }
 
-    public static function sanitize_data(mixed $data): mixed
+    public static function sanitizeData(mixed $data): mixed
     {
         if (is_array($data)) {
             foreach ($data as $key => $value) {
-                $data[$key] = self::sanitize_data($value);
+                $data[$key] = self::sanitizeData($value);
             }
             return $data;
         } elseif (is_numeric($data)) {
-            return is_float($data) ? self::sanitize_float($data) : self::sanitize_integer($data);
+            return is_float($data) ? self::sanitizeFloat($data) : self::sanitizeInteger($data);
         } elseif (is_email($data)) {
-            return self::sanitize_email($data);
-        } elseif (self::is_url($data)) {
-            return self::sanitize_url($data);
+            return self::sanitizeEmail($data);
+        } elseif (self::isUrl($data)) {
+            return self::sanitizeUrl($data);
         } elseif (is_string($data)) {
-            return self::sanitize_text($data);
+            return self::sanitizeText($data);
         } else {
             // We couldn't find a proposer sanitization method, so we leave the data as is
             return $data;
@@ -64,10 +69,10 @@ class Sanitizer implements SingletonInterface
 
     public function sanitize(mixed $data): mixed
     {
-        return self::sanitize($data);
+        return self::sanitizeData($data);
     }
 
-    protected static function is_url($url): bool|int
+    protected static function isUrl($url): bool|int
     {
         return preg_match('/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/', $url);
     }
